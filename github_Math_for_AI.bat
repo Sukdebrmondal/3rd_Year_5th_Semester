17c1fbda58ec8300436301df2b461a56f5966cb5
Suku <sukdebdhalla@gmail.com>
2025-07-11 08:34:05 +0530

    Software_engineering

--- github_Math_for_AI.bat
+++ github_Math_for_AI.bat
@@ -2,5 +2,5 @@ git pull origin main
 git status
 git add --all
 git status
-git commit -m "SQL"
+git commit -m "Software_engineering"
 git push origin main
--- github_software_engineering.bat
+++ github_software_engineering.bat
@@ -2,5 +2,5 @@ git pull origin main
 git status
 git add --all
 git status
-git commit -m "Automata/pdf"
+git commit -m "Software_engineering"
 git push origin main
